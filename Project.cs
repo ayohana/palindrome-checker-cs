700ab070ba73ebc134e49cf08b823e22c7d40d54
Adela Darmansyah and Sasa Schwartz <sasa@>
2020-02-25 09:23:39 -0800

    complete IsPalindrome fx recursively

--- Project.cs
+++ Project.cs
@@ -1,10 +1,35 @@
 using System;
 using System.Collections.Generic;
 
-class Project
+public class Project
 {
-  static void Main()
+  public static bool IsPalindrome(string input) 
   {
-    // project code goes here
+    int length = input.Length;
+    if (length <= 1)
+    {
+      return true;
+    }
+    if (input[0] == input[length - 1])
+    {
+      return IsPalindrome(input.Substring(1,length - 2));
+    }
+    else {
+      return false;
+    }
+  }
+
+  public static void Main()
+  {
+    Console.WriteLine("Enter a word");
+    string input = Console.ReadLine();
+    if (IsPalindrome(input)) 
+    {
+      Console.WriteLine(input + " is a palindrome.");
+    } 
+    else 
+    {
+      Console.WriteLine(input + " is not a palindrome.");
+    }
   }
 }
